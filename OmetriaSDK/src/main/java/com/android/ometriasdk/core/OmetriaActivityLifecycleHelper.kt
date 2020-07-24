43183bea686c13097e8548a6f8437b3894380be2
Cristian Dregan <cristi.dregan@tapptitude.com>
2020-07-24 11:58:39 +0300

    Track deep link event

--- OmetriaSDK/src/main/java/com/android/ometriasdk/core/OmetriaActivityLifecycleHelper.kt
+++ OmetriaSDK/src/main/java/com/android/ometriasdk/core/OmetriaActivityLifecycleHelper.kt
@@ -12,17 +12,15 @@ import java.util.concurrent.atomic.AtomicBoolean
  * on 07/07/2020.
  */
 
-private val TAG = OmetriaActivityLifecycleHelper::class.simpleName
-
 class OmetriaActivityLifecycleHelper :
-    Application.ActivityLifecycleCallbacks,
-    DefaultLifecycleObserver {
+    DefaultLifecycleObserver,
+    Application.ActivityLifecycleCallbacks {
 
     private val firstLaunch = AtomicBoolean(false)
     private val trackedApplicationLifecycleEvents = AtomicBoolean(false)
 
     /**
-     * Using lifecycle's observer onCreate callback to track Application Opened event
+     * Using lifecycle's observer onCreate callback to decide if is first launch
      */
     override fun onCreate(owner: LifecycleOwner) {
         if (!trackedApplicationLifecycleEvents.getAndSet(true)) {
@@ -31,38 +29,49 @@ class OmetriaActivityLifecycleHelper :
     }
 
     /**
-     * Using lifecycle's observer onStart callback to track Application Opened event
+     * Using lifecycle's observer onStart callback to track Launch Application event
+     * and Bring Application to Foreground event
      */
     override fun onStart(owner: LifecycleOwner) {
         if (firstLaunch.get()) {
-            OmetriaLog.d(TAG, "${owner.lifecycle.currentState}, First launch")
+            Ometria.instance().trackEvent(OmetriaEventType.LAUNCH_APPLICATION)
         }
 
         !firstLaunch.getAndSet(false)
 
-        OmetriaLog.d(TAG, "${owner.lifecycle.currentState}, Application opened")
-
-        // ToDO track event
+        Ometria.instance().trackEvent(OmetriaEventType.BRING_APPLICATION_TO_FOREGROUND)
     }
 
     /**
      * Using lifecycle's observer onStop callback to track Application Backgrounded event
      */
     override fun onStop(owner: LifecycleOwner) {
-        OmetriaLog.d(TAG, "${owner.lifecycle.currentState}, Application in background")
-
-        // ToDO track event
+        Ometria.instance().trackEvent(OmetriaEventType.SEND_APPLICATION_TO_BACKGROUND)
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////
 
     /**
-     * Using activity created callback to track Screen View event
+     * Using activity created callback to track Open Deep Link event
      */
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-        OmetriaLog.d(TAG, activity.localClassName)
+        trackDeepLink(activity)
+    }
 
-        // ToDO track event
+    private fun trackDeepLink(activity: Activity) {
+        val intent = activity.intent
+        if (intent == null || intent.data == null) {
+            return
+        }
+
+        Ometria.instance().trackEvent(OmetriaEventType.OPEN_DEEP_LINK)
+    }
+
+    /**
+     * Using activity started callback to track Screen View event
+     */
+    override fun onActivityStarted(activity: Activity) {
+        Ometria.instance().trackEvent(OmetriaEventType.VIEW_SCREEN, activity::class.simpleName)
     }
 
     /**
@@ -70,7 +79,6 @@ class OmetriaActivityLifecycleHelper :
      */
     override fun onActivityResumed(activity: Activity) {}
     override fun onActivityPaused(activity: Activity) {}
-    override fun onActivityStarted(activity: Activity) {}
     override fun onActivityStopped(activity: Activity) {}
     override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
     override fun onActivityDestroyed(activity: Activity) {}
