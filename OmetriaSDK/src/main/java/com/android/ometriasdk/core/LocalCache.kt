ef43c1276a15fb8e3e1ebce0f783843007ad4889
Cristian Dregan <cristi.dregan@tapptitude.com>
2020-07-27 16:15:20 +0300

    Add support for cache installment ID.

--- OmetriaSDK/src/main/java/com/android/ometriasdk/core/LocalCache.kt
+++ OmetriaSDK/src/main/java/com/android/ometriasdk/core/LocalCache.kt
@@ -9,16 +9,25 @@ import android.content.SharedPreferences
  */
 
 private const val LOCAL_CACHE_PREFERENCES = "LOCAL_CACHE_PREFERENCES"
-private const val IS_FIRST_APP_RUN = "IS_FIRST_APP_RUN"
+private const val IS_FIRST_APP_RUN_KEY = "IS_FIRST_APP_RUN_KEY"
+private const val INSTALLMENT_ID_KEY = "INSTALLMENT_ID_KEY"
 
 internal class LocalCache(private val context: Context) {
 
     fun isFirstAppRun(isFirstAppRun: Boolean) {
-        getSettingsPreferences().edit().putBoolean(IS_FIRST_APP_RUN, isFirstAppRun).apply()
+        getSettingsPreferences().edit().putBoolean(IS_FIRST_APP_RUN_KEY, isFirstAppRun).apply()
     }
 
     fun isFirstAppRun(): Boolean {
-        return getSettingsPreferences().getBoolean(IS_FIRST_APP_RUN, true)
+        return getSettingsPreferences().getBoolean(IS_FIRST_APP_RUN_KEY, true)
+    }
+
+    fun saveInstallmentID(installmentID: String) {
+        getSettingsPreferences().edit().putString(INSTALLMENT_ID_KEY, installmentID).apply()
+    }
+
+    fun getInstallmentID(): String? {
+        return getSettingsPreferences().getString(INSTALLMENT_ID_KEY, null)
     }
 
     private fun getSettingsPreferences(): SharedPreferences {
