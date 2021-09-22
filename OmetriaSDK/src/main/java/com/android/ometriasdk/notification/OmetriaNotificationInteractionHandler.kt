23237e17a8dc6ef49492c3c2ed3dea5b533b9bbf
Cristian Dregan <cristi.dregan@tapptitude.com>
2021-09-22 14:55:00 +0300

    Keep onDeepLinkingInteraction method but make it deprecated

--- OmetriaSDK/src/main/java/com/android/ometriasdk/notification/OmetriaNotificationInteractionHandler.kt
+++ OmetriaSDK/src/main/java/com/android/ometriasdk/notification/OmetriaNotificationInteractionHandler.kt
@@ -9,5 +9,10 @@ package com.android.ometriasdk.notification
  * An interface that allows you to control what happens when a user interacts with an Ometria originated push notification
  */
 interface OmetriaNotificationInteractionHandler {
-    fun onNotificationInteraction(ometriaNotification: OmetriaNotification)
+    @Deprecated(
+        message = "Use the new onNotificationInteraction(ometriaNotification: OmetriaNotification) method",
+    )
+    fun onDeepLinkInteraction(deepLink: String) {/* default implementation */ }
+
+    fun onNotificationInteraction(ometriaNotification: OmetriaNotification) {/* default implementation */ }
 }
