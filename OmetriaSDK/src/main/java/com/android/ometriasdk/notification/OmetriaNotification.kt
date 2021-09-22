17a70da9cea60b1879dbb5aa2a1c2552a7d992c5
Cristian Dregan <cristi.dregan@tapptitude.com>
2021-09-22 15:17:36 +0300

    Add java docs for OmetriaNotification data class

--- OmetriaSDK/src/main/java/com/android/ometriasdk/notification/OmetriaNotification.kt
+++ OmetriaSDK/src/main/java/com/android/ometriasdk/notification/OmetriaNotification.kt
@@ -3,12 +3,14 @@ package com.android.ometriasdk.notification
 /**
  * An object that exposes the content of a received notification.
  *
- * @param deepLink
- * @param imageUrl
- * @param campaignType
- * @param externalCustomerId
- * @param sendId
- * @param tracking
+ * @param deepLink The URL that was sent in the notification. We append tracking parameters to the URL
+ * specified in the account and campaign settings (these can be changed in the Ometria app).
+ * @param imageUrl The image URL that was sent in the notification.
+ * @param campaignType Can be trigger, mass, transactional (currently only trigger is used).
+ * @param externalCustomerId The id of the contact that was specified at customer creation (in the mobile app) or ingesting to Ometria.
+ * @param sendId Unique id of the message.
+ * @param tracking A map that contains all tracking fields specified in the account and campaign
+ * settings (can be changed in the Ometria app, uses some defaults if not specified).
  */
 data class OmetriaNotification(
     val deepLink: String?,
