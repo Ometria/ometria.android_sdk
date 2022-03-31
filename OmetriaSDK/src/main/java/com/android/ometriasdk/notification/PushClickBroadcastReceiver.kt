a17784fa23c32b746ad8ea5e386b21c4f5f6e3b3
Cristian Dregan <cristi.dregan@tapptitude.com>
2022-03-31 12:24:48 +0300

    Small refactoring

--- OmetriaSDK/src/main/java/com/android/ometriasdk/notification/PushClickBroadcastReceiver.kt
+++ OmetriaSDK/src/main/java/com/android/ometriasdk/notification/PushClickBroadcastReceiver.kt
@@ -3,7 +3,6 @@ package com.android.ometriasdk.notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.webkit.URLUtil
 import com.android.ometriasdk.core.Ometria
 import com.android.ometriasdk.core.event.OmetriaEventType
 import com.android.ometriasdk.core.network.toOmetriaNotification
