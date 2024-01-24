25a3134e9f80c756fa280d807349ea3a45e6452b
Cristian Dregan <cristi.dregan@tapptitude.com>
2024-01-24 16:02:47 +0200

    Update sample app version to 1.1.0 (10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.android.sample"
         minSdk = 23
         targetSdk = 33
-        versionCode = 9
-        versionName = "1.0.9"
+        versionCode = 10
+        versionName = "1.1.0"
     }
     signingConfigs {
         getByName("debug") {
