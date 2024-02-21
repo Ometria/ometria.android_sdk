bbea3961fc6d5ab949f1394cd0774cf982293226
Cristian Dregan <cristi.dregan@tapptitude.com>
2024-02-21 15:24:48 +0200

    Sample app version 1.2.0 (11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.android.sample"
         minSdk = 23
         targetSdk = 33
-        versionCode = 10
-        versionName = "1.1.0"
+        versionCode = 11
+        versionName = "1.2.0"
     }
     signingConfigs {
         getByName("debug") {
