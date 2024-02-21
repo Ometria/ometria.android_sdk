b13d7bbf816bbb787b74b3892d3720d492440647
Cristian Dregan <cristi.dregan@tapptitude.com>
2024-02-21 16:42:22 +0200

    Add documentation on OmetriaMessagingReceiver

--- OmetriaSDK/src/main/java/com/android/ometriasdk/notification/OmetriaMessagingReceiver.kt
+++ OmetriaSDK/src/main/java/com/android/ometriasdk/notification/OmetriaMessagingReceiver.kt
@@ -5,10 +5,16 @@ import android.content.Context
 import android.content.Intent
 import com.android.ometriasdk.core.Ometria
 import com.android.ometriasdk.core.Ometria.Companion.clearOldInstance
+import com.android.ometriasdk.core.event.OmetriaEventType
 import com.android.ometriasdk.core.network.toOmetriaNotificationBody
 import com.google.firebase.messaging.RemoteMessage
 
-
+/**
+ * Used as Push notification interceptor.
+ * This will not replace Firebase's own BroadcastReceiver.
+ * The purpose of using this BroadcastReceive is to log [OmetriaEventType.NOTIFICATION_RECEIVED] event
+ * when the app is killed for React Native use-case.
+ */
 class OmetriaMessagingReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
