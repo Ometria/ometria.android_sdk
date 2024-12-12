4c795fc2dbcb21ed5172b7d24e528e2564765130
Cristian Dregan <cristi.dregan@tapptitude.com>
2024-12-12 13:24:25 +0200

    Update app compat library

--- OmetriaSDK/build.gradle.kts
+++ OmetriaSDK/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     // Android Core
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.security:security-crypto:1.0.0")
 
     // Lifecycle
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     // Support libraries
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.work:work-runtime:2.8.1")
