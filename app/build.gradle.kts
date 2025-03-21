63f16efddcc8294f011fcbfadef9d3bc8b886f8b
George Fodor <georgefodor2013@gmail.com>
2025-03-21 15:06:43 +0200

    increase sample app version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.android.sample"
         minSdk = 23
         targetSdk = 35
-        versionCode = 22
+        versionCode = 23
         versionName = "1.10.0"
     }
     signingConfigs {
